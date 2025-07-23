2b98e1a1f64990dee31c9c17f88f5af5a5920105
Matthew Miller <mwm@fastmail.com>
2025-07-23 09:53:15 +0200

    Begin collection of card art

---
+++ lib/hscards/art.ex
@@ -0,0 +1,68 @@
+defmodule HSCards.Art do
+  # There is more than one way to get the full art URL for a card, even
+  # ignoring localization.
+  # We're going to focus on what works for us and let the rest sort itself out.
+  require Logger
+  @styles ["tiles", "256x"]
+  @base_url "https://art.hearthstonejson.com/v1"
+  @base_dir :code.priv_dir(:hscards) |> Path.join("images")
+
+  @doc """
+  Available art styles.
+  """
+  def styles, do: @styles
+
+  def by_card(card, style \\ "tiles")
+
+  def by_card(card, style) when style in @styles do
+    case card do
+      %{id: id} when is_binary(id) -> file_by_id(id, style)
+      id when is_binary(id) -> file_by_id(id, style)
+      _ -> raise ArgumentError, "Invalid card format: #{inspect(card)}"
+    end
+  end
+
+  def by_card(_, style) do
+    raise ArgumentError,
+          "Invalid style: #{style}. Available styles are: #{Enum.join(@styles, ", ")}"
+  end
+
+  defp path(id, style, where \\ @base_dir), do: where <> "/#{style}/#{id}.jpg"
+
+  defp file_by_id(id, style) do
+    case File.read(path(id, style)) do
+      {:ok, contents} ->
+        contents
+
+      {:error, :enoent} ->
+        # If the file doesn't exist, we can try to fetch it from the web.
+        Task.start(fn ->
+          load_art(id, style)
+        end)
+
+        file_by_id("default", style)
+    end
+  end
+
+  defp load_art(id, style) do
+    with {:ok, {{_, 200, _}, _headers, img_data}} <-
+           :httpc.request(path(id, style, @base_url)) do
+      file_path = path(id, style)
+
+      # Ensure the directory exists
+      File.mkdir_p!(Path.dirname(file_path))
+
+      # Write the file to disk
+      case File.write(file_path, img_data) do
+        :ok -> Logger.info("Art file #{file_path} written successfully.")
+        {:error, reason} -> Logger.warning("Failed to write art file #{file_path}: #{reason}")
+      end
+    else
+      {:ok, {{_, status_code, _}, _headers, _}} ->
+        Logger.warning("Failed to fetch art for #{id} in style #{style}: HTTP #{status_code}")
+
+      {:error, reason} ->
+        Logger.warning("Failed to fetch art for #{id} in style #{style}: #{reason}")
+    end
+  end
+end
